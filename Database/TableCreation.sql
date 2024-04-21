5d236532d8d146ce3a9364bd4c0adac563955fb5
Dhairya Mehra <mpdhairyamehra000942@gmail.com>
2024-04-21 19:31:49 +0530

    Update TableCreation.sql

--- Database/TableCreation.sql
+++ Database/TableCreation.sql
@@ -13,3 +13,12 @@ create table order_status (
 status_id int primary key auto_increment,
 status_name bool
 );
+
+create table orders (
+order_id int primary key auto_increment,
+order_date_time datetime,
+total_amount int,
+payment_status bool,
+status_id int,
+foreign key(status_id) references order_status(status_id)
+);
